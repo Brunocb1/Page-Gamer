b465c361d35b1638ae6b71f397514421fc1f38f7
Bruno Santos <brunohsantos1993@gmail.com>
2024-01-30 00:31:34 -0300

    PageGamer

---
+++ index.js
@@ -0,0 +1,8 @@
+function openForm(){
+    document.querySelector("form").style.opacity = 1;
+    document.querySelector("section").style.opacity = 0.2;
+}
+function closeForm(){
+    document.querySelector("form").style.opacity = 0;
+    document.querySelector("section").style.opacity = 1;
+}
